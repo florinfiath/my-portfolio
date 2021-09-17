768c33bd59fd40801570fd171e66cb74f303c8b4
Florin Fiath <fiathf@yahoo.com>
2021-09-17 13:42:52 +0200

    deploy

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import './Footer.css'
+
+const Footer = () => (
+  <footer className='footer'>
+    <a
+      href='https://florinfiath.github.io/my-portfolio'
+      className='link footer__link'
+    >
+      Created By Florin Fiath
+    </a>
+  </footer>
+)
+
+export default Footer
---
+++ src/my-portfolio.js
@@ -0,0 +1,56 @@
+const header = {
+    homepage: 'https://florinfiath.github.io/portfolio',
+    title: 'FF',
+  }
+  
+  const about = {
+    name: 'Florin Fiath',
+    role: 'Full Stack Web Developer',
+    description:'An engineer with more than 10 years experience in deployment and rollout management who decided to take another challenge and learn something new in the programming world.',
+    resume: 'https://drive.google.com/file/d/12CwUyN6r8kTp14ApqDHCEAYs95o0qy1M/view?usp=sharing',
+    lebenslauf:'https://drive.google.com/file/d/13621mvCec8KWrael_ufh0h3FZgfVrib9/view?usp=sharing',
+    social: {
+      linkedin: 'https://www.linkedin.com/in/florin-fiath-20637827/',
+      github: 'https://github.com/florinfiath',
+    },
+  }
+  
+  const projects = [
+    {
+      name: 'DCI DEVS',
+      description:
+        'DCI Developer Community ',
+      stack: ['HTML', 'CSS', 'React', 'Node.js', 'Express.js','MongoDB'],
+      sourceCode: 'https://github.com/florinfiath/Final-Project-DCI-Developer-Community-Frontend ',
+      livePreview: 'https://dci-devs.herokuapp.com/',
+    },
+    {
+      name: 'S.F.K Entertainment',
+      description:
+        'React Final Project',
+      stack: ['CSS', 'React', 'Node.js', 'Express.js','MongoDB'],
+      sourceCode: 'https://github.com/florinfiath/React_Final_Project',
+      livePreview: 'https://sfk-entertainment.herokuapp.com/',
+    }
+  ]
+  
+  const skills = [
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'MongoDB',
+    'Express.js',
+    'React',
+    'Node.js',
+    'Redux',
+    'SASS',
+    'Material UI',
+    'Git',
+  ]
+  
+  const contact = {
+    email: 'fiathf@gmail.com',
+  }
+  
+  export { header, about, projects, skills, contact }
+  
